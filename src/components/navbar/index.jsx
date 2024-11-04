3acd6c27b2b1876d85dcb3a0f9373e864b4baed9
Eriyadan Febina <febinaeriyadan@gmail.com>
2024-11-04 11:00:30 +0800

    Basic nav-bar functions created

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import {FaBars, FaReact} from 'react-icons/fa';
+import {HiX} from 'react-icons/hi'
+import {Link} from 'react-router-dom'
+
+
+const data= [ 
+    {
+        label: 'Home',
+        to :'/'
+    },
+    {
+        label:'About Me',
+        to : '/about'
+
+    },
+    {
+        label:'SKILLS',
+        to : '/skills'
+
+    },
+    {
+        label:'PORTFOLIO',
+        to : '/portfolio'
+
+    },
+    {
+        label:'RESUME',
+        to : '/resume'
+
+    },
+    {
+        label:'CONTACT',
+        to : '/contact'
+
+    },
+
+]
+
+const Navbar=()=>{
+    const [toggleIcon, setToggleIcon] = useState(false)
+    const handleToggleIcon = ()=>{
+      setToggleIcon(!toggleIcon);
+    };
+    return(
+        <div>
+         <nav className="navbar">
+            <div className="navbar__container">
+           <Link to={'/'} className="navbar__container__logo">
+           <FaReact size={30}/>
+           </Link>
+          
+
+            </div>
+            <ul className="navbar__containe__menu">
+                {
+                   data.map((item,key)=> (
+                    <li key={key} className="navbar__container__menu__item">
+                        <Link className="navbar__container__menu__item__links" to={item.to}>
+                        {item.label}
+
+                        </Link>
+
+                    </li>
+                   ))
+                }
+
+            </ul>
+            <div className="nav-icon" onClick={handleToggleIcon}>
+                {
+                    toggleIcon ? <HiX size={30}/> : <FaBars size={30}/>
+                }
+
+            </div>
+
+
+         </nav>
+        </div>
+    )
+}
+
+export default Navbar;
