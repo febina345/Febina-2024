fac56a37b0af03bb6431422dbb4cf16bb01a0b9a
Eriyadan Febina <febinaeriyadan@gmail.com>
2024-11-05 08:47:51 +0800

    Resume and skills section updated

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,48 @@
+export const data = {
+    experience: [
+        {
+            title: "Full Stack Developer",
+            subTitle: "Singapore",
+            description: "User Experience,frontend and Back end"
+        },
+        {
+            title: "Front End Developer",
+            subTitle: "Singapore",
+            description: "UI development"
+
+        },
+        {
+            title:"Project Management",
+            subTitle:"Singapore",
+            description:"Jira scrum master"
+        },
+        {
+            title:"software tester",
+            subTitle:"Singapore",
+            description:"Machine tester, manual tester"
+        },
+        {
+            title:"System analyst",
+            subTitle:"Singapore",
+            description:"Microsoft Azure Cloud"
+        }
+    ],
+    education:[
+        {
+            title:"Bachelor of Science(Hons) in Computer Science",
+            subTitle:"University of Essex",
+            description:"Singapore",
+        },
+        {
+            title:"BSc(Hons)Science",
+            subTitle:"KAU,Kerala",
+            description:"INDIA"
+        },
+        {
+            title:"Seconday",
+            subTitle:"PKMMHSS",
+            description:"INDIA"
+
+        },
+    ],
+};
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,79 @@
+export const skillsData = [
+    {
+        label:"FRONT END",
+        data: [
+            {
+                skillsName: "HTML",
+                percentage:"90",
+            },
+            {
+                skillsName:"CSS",
+                percentage :"70",
+            },
+            {
+                skillsName:"JAVASCRIPT",
+                percentage:"80",
+            },
+            {
+                skillsName: "REACT JS",
+                percentage: "80",
+            },
+            {
+                skillsName:"ANGULAR",
+                percentage:"50"
+            },
+        ],
+
+    },
+    {
+        label:"BACK END",
+        data: [
+            {
+                skillsName:"JAVA",
+                percentage:"65",
+            },
+            {
+                skillsName:"PYTHON",
+                percentage:"70",
+            },
+            {
+                skillsName:"C",
+                percentage:"50",
+            },
+            {
+                skillsName:"C++",
+                percentage:"50",
+            },
+            {
+                skillsName:".Net/C#",
+                percentage:"50"
+            },
+            {
+                skillsName:"NodeJs/Express",
+                percentage:"70"
+            }
+        ],
+    },
+    {
+        label:"DATABASES",
+        data:[
+            {
+                skillsName:"SQL",
+                percentage:"80",
+            },
+            {
+                skillsName:"MONGO DB",
+                percentage:"70",
+            },
+            {
+                skillsName:"ORACLE",
+                percentage:"60"
+            },
+            {
+                skillsName:"POSTGRESQL",
+                percentage:"50"
+            },
+        ],
+    },
+
+];
